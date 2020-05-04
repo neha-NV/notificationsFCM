e3bfe01df8239709c3296fbd6c49dcdc9c46826f
Neha Nanhi <neha@gmail.com>
2020-05-04 16:59:03 +0530

    Changes from folder explorer

--- app/src/main/java/com/example/notificationsfcm/MainActivity.java
+++ app/src/main/java/com/example/notificationsfcm/MainActivity.java
@@ -13,6 +13,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -61,5 +62,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void gullu() {
+        Toast.makeText(this, "Viku", Toast.LENGTH_SHORT).show();
     }
 }
